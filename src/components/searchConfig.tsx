5e2ea1cfc3405155274ccf2384dfda5b843bcda3
rkeerthient <rkeerthi@yext.com>
2024-03-29 18:54:01 -0400

    added chat packages

--- src/components/searchConfig.tsx
+++ src/components/searchConfig.tsx
@@ -1,10 +1,14 @@
 import { Environment } from "@yext/search-headless-react";
-
-const searchConfig = {
+import { ChatConfig } from "@yext/chat-headless-react";
+export const searchConfig = {
   apiKey: import.meta.env.YEXT_PUBLIC_API_KEY,
   experienceKey: import.meta.env.YEXT_PUBLIC_EXP_KEY,
   locale: "ja",
   environment: Environment.SANDBOX,
 };
 
-export default searchConfig;
+export const chatConfig: ChatConfig = {
+  botId: import.meta.env.YEXT_PUBLIC_CHAT_BOT_ID,
+  apiKey: import.meta.env.YEXT_PUBLIC_CHAT_API_KEY,
+  env: "SANDBOX",
+};
