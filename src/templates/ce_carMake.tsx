7dff6f87207720de9ab183bcea71c415ff1f0c1f
rkeerthient <rkeerthi@yext.com>
2024-04-02 20:35:24 -0400

    test update

---
+++ src/templates/ce_carMake.tsx
@@ -0,0 +1,135 @@
+/**
+ * This is an example of how to create a template that makes use of streams data.
+ * The stream data originates from Yext's Knowledge Graph. When a template in
+ * concert with a stream is built by the Yext Pages system, a static html page
+ * is generated for every corresponding (based on the filter) stream document.
+ *
+ * Another way to think about it is that a page will be generated using this
+ * template for every eligible entity in your Knowledge Graph.
+ */
+
+import {
+  GetHeadConfig,
+  GetPath,
+  GetRedirects,
+  HeadConfig,
+  Template,
+  TemplateConfig,
+  TemplateProps,
+  TemplateRenderProps,
+  TransformProps,
+} from "@yext/pages";
+import "../index.css";
+import DirectoryCityGrid from "../components/DirectoryCityGrid";
+import Breadcrumbs from "../components/Breadcrumbs";
+import PageLayout from "../components/page-layout";
+
+export const config: TemplateConfig = {
+  stream: {
+    $id: "carmake-stream",
+    filter: {
+      entityTypes: ["ce_carManual"],
+    },
+    fields: [
+      "id",
+      "uid",
+      "meta",
+      "name",
+      "description",
+      "slug",
+      "photoGallery",
+      "dm_directoryParents_toyota_manual_final.name",
+      "dm_directoryParents_toyota_manual_final.slug",
+      "dm_directoryParents_toyota_manual_final.meta",
+      "dm_directoryParents_toyota_manual_final.photoGallery",
+      "dm_directoryChildren.name",
+      "dm_directoryChildren.slug",
+      "dm_directoryChildren.photoGallery",
+    ],
+    localization: {
+      locales: ["ja"],
+      primary: false,
+    },
+  },
+};
+
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+  return `${document.slug.toString()}`;
+};
+
+export const getRedirects: GetRedirects<TemplateProps> = ({ document }) => {
+  return [`alias/${document.locale}/${document.id.toString()}`];
+};
+
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  document,
+}): HeadConfig => {
+  return {
+    title: document.name,
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+    tags: [
+      {
+        type: "link",
+        attributes: {
+          rel: "icon",
+          type: "image/x-icon",
+        },
+      },
+    ],
+  };
+};
+
+export const transformProps: TransformProps<any> = async (data) => {
+  const { dm_directoryParents_toyota_manual_final, name } = data.document;
+
+  (dm_directoryParents_toyota_manual_final || []).push({
+    name: name,
+    slug: "",
+  });
+
+  return {
+    ...data,
+    document: {
+      ...data.document,
+      dm_directoryParents_toyota_manual_final:
+        dm_directoryParents_toyota_manual_final,
+    },
+  };
+};
+
+const CarMake: Template<TemplateRenderProps> = ({
+  relativePrefixToRoot,
+  document,
+}) => {
+  const {
+    name,
+    description,
+    siteDomain,
+    dm_directoryParents_toyota_manual_final,
+    dm_directoryChildren,
+    photoGallery,
+  } = document;
+
+  return (
+    <>
+      <PageLayout>
+        <div className="centered-container">
+          <Breadcrumbs
+            breadcrumbs={dm_directoryParents_toyota_manual_final}
+            baseUrl={relativePrefixToRoot}
+          />
+          {dm_directoryChildren.name}
+          <DirectoryCityGrid
+            name={name}
+            description={description}
+            directoryChildren={dm_directoryChildren}
+            relativePrefixToRoot={relativePrefixToRoot}
+          />
+        </div>
+      </PageLayout>
+    </>
+  );
+};
+
+export default CarMake;
--- src/templates/root.tsx
+++ src/templates/root.tsx
@@ -23,12 +23,14 @@ import {
   VerticalResults,
   executeSearch,
 } from "@yext/search-ui-react";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import DirectoryRootGrid from "../components/DirectoryRootGrid";
 import ManualRegular from "../components/cards/ManualRegular";
 import PageLayout from "../components/page-layout";
 import "../index.css";
 import { useEffect } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { ChevronUpDownIcon, CheckIcon } from "@heroicons/react/20/solid";
 
 export const config: TemplateConfig = {
   stream: {
@@ -96,11 +98,25 @@ const Inner = ({ dm_directoryChildren, relativePrefixToRoot }: any) => {
   const [showDir, setShowDir] = useState<boolean>(true);
   const searchActions = useSearchActions();
   useSearchState((state) => state.filters);
-
+  const [customModelFacets, setCustomModelFacets] = useState([]);
+  const [customYearFacets, setCustomYearFacets] = useState([]);
   const isLoading =
     useSearchState((state) => state.searchStatus.isLoading) || false;
+  const [selected, setSelected] = useState(customModelFacets[0]);
+  console.log(useSearchState((state) => state.filters.facets));
   useEffect(() => {
+    console.log(`anain`);
+
     searchActions.setVertical("manual");
+    searchActions
+      .executeVerticalQuery()
+      .then((res) =>
+        setCustomModelFacets(
+          res?.facets
+            ?.filter((item) => item.fieldId === "c_carmodel")
+            .flatMap((item) => item.options.map((option) => option.displayName))
+        )
+      );
   }, []);
 
   useEffect(() => {
@@ -112,10 +128,84 @@ const Inner = ({ dm_directoryChildren, relativePrefixToRoot }: any) => {
     }
   }, [searchActions.state.filters.facets]);
 
+  useEffect(() => {
+    console.log(`entered`);
+
+    searchActions.setQuery(selected);
+    searchActions
+      .executeVerticalQuery()
+      .then((res) =>
+        console.log(useSearchState((state) => state.filters.facets))
+      );
+  }, [selected]);
+
   return (
     <div className="!my-4 space-y-2 pb-12 px-10 bg-[#f2f2f2]">
       <>
         <div className="flex justify-between gap-8">
+          {/*Filter 1 */}
+
+          {/* {customModelFacets && (
+            <div className="fixed top-16 w-72">
+              <Listbox value={selected} onChange={setSelected}>
+                <div className="relative mt-1">
+                  <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block truncate">{selected}</span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <ChevronUpDownIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                      {customModelFacets.map((person, personIdx) => {
+                        return (
+                          <Listbox.Option
+                            key={personIdx}
+                            className={({ active }) =>
+                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? "bg-amber-100 text-amber-900"
+                                  : "text-gray-900"
+                              }`
+                            }
+                            value={person}
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? "font-medium" : "font-normal"
+                                  }`}
+                                >
+                                  {person}
+                                </span>
+                                {selected ? (
+                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                    <CheckIcon
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        );
+                      })}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+            </div>
+          )} */}
           <FilterSearch
             placeholder="車種を検索"
             searchOnSelect={true}
